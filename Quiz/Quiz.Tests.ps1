7334f65b99187d0cfd8df1b1584ac6281af19fa5
Yang Rui <ryang@thoughtworks.com>
2014-05-09 16:55:43 +0800

    refactor the function for judging times of number

--- Quiz/Quiz.Tests.ps1
+++ Quiz/Quiz.Tests.ps1
@@ -4,14 +4,18 @@
 Describe "Quiz" {
 
     It "should return True if the number is times of 3" {
-        NumberIsTimesOf3 3 | Should Be $True
+        NumberIsTimesOfSpecified -num 3 -specified 3 | Should Be $True
     }
 
 	It "should return False if the number is NOT times of 3" {
-		NumberIsTimesOf3 1 | Should Be $False
+		NumberIsTimesOfSpecified -num 7 -specified 3 | Should Be $False
 	}
 
     It "should return True if the number is times of 5" {
-        NumberIsTimesOf5 5 | Should Be $True
+        NumberIsTimesOfSpecified -num 10 -specified 5 | Should Be $True
+    }
+
+    It "should return False if the number is NOT times of 5" {
+        NumberIsTimesOfSpecified -num 3 -specified 5 | Should Be $False
     }
 }
--- Quiz/Quiz.ps1
+++ Quiz/Quiz.ps1
@@ -1,18 +1,8 @@
-﻿function NumberIsTimesOf3($num) {
-    if($num % 3 -eq 0){
+﻿function NumberIsTimesOfSpecified ($num, $specified){
+    if($num % $specified -eq 0){
         return $true;
     }
     else{
         return $false;
     }
 }
-
-function NumberIsTimesOf5($num) {
-    if($num % 5 -eq 0){
-        return $true;
-    }
-    else{
-        return $false;
-    }
-}
-
