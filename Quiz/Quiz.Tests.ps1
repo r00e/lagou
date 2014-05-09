1554248b86c4d6be80b5e3fc22dcded27cea37a7
Yang Rui <ryang@thoughtworks.com>
2014-05-09 16:51:02 +0800

    test times of 3 and 5

---
+++ Quiz/Quiz.Tests.ps1
@@ -0,0 +1,17 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$here\Quiz.ps1"
+
+Describe "Quiz" {
+
+    It "should return True if the number is times of 3" {
+        NumberIsTimesOf3 3 | Should Be $True
+    }
+
+	It "should return False if the number is NOT times of 3" {
+		NumberIsTimesOf3 1 | Should Be $False
+	}
+
+    It "should return True if the number is times of 5" {
+        NumberIsTimesOf5 5 | Should Be $True
+    }
+}
---
+++ Quiz/Quiz.ps1
@@ -0,0 +1,18 @@
+﻿function NumberIsTimesOf3($num) {
+    if($num % 3 -eq 0){
+        return $true;
+    }
+    else{
+        return $false;
+    }
+}
+
+function NumberIsTimesOf5($num) {
+    if($num % 5 -eq 0){
+        return $true;
+    }
+    else{
+        return $false;
+    }
+}
+
