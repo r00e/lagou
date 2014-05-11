769870ab7536875fbeafccb0b7882a075a684783
Yang Rui <ryang@thoughtworks.com>
2014-05-11 11:16:18 +0800

    say fizz for times of 3

--- Quiz/Quiz.Tests.ps1
+++ Quiz/Quiz.Tests.ps1
@@ -6,4 +6,8 @@ Describe "Quiz" {
     It "should return number itself" {
         Say 1 | Should Be 1
     }
+
+    It "should return Fizz for the times of 3"{
+        Say 3 | Should Be "Fizz"
+    }    
 }
--- Quiz/Quiz.ps1
+++ Quiz/Quiz.ps1
@@ -1,3 +1,22 @@
 ﻿function Say($num){
-    return $num
+    $result = SayFizzForTimesOf3($num)
+    return $result
+}
+
+function SayFizzForTimesOf3($num){
+    if(IsTimesOf3($num)){
+        return "Fizz"
+    }
+    else{
+        return $num
+    }
+}
+
+function IsTimesOf3($num){
+    if( ($num % 3) -eq 0){
+        return $true
+    }
+    else{
+        return $false
+    }
 }
