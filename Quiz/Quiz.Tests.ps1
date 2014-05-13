cfb4ee3dac285275e13492a305380fe43d73f6c6
Yang Rui <ryang@thoughtworks.com>
2014-05-14 07:58:17 +0800

    make test to cover more cases

--- Quiz/Quiz.Tests.ps1
+++ Quiz/Quiz.Tests.ps1
@@ -9,29 +9,36 @@ Describe "Quiz" {
 
     It "should return Fizz for the times of 3"{
         Say 3 | Should Be "Fizz"
+        Say 6 | Should Be "Fizz"
     }
 
     It "should return Buzz for the times of 5"{
         Say 5 | Should Be "Buzz"
+        Say 10 | Should Be "Buzz"
     }
 
     It "should return Whizz for the times of 7"{
         Say 7 | Should Be "Whizz"
+        Say 14 | Should Be "Whizz"
     }
 
     It "should return FizzBuzz for the times of both 3 and 5"{
         Say 15 | Should Be "FizzBuzz"
+        Say 30 | Should Be "FizzBuzz"
     }   
 
     It "should return FizzWhizz for the times of both 3 and 7"{
         Say 21 | Should Be "FizzWhizz"
+        Say 189 | Should Be "FizzWhizz"
     }    
 
     It "should return BuzzWhizz for the times of both 5 and 7"{
         Say 35 | Should Be "BuzzWhizz"
+        Say 70 | Should Be "BuzzWhizz"
     }    
 
     It "should return FizzBuzzWhizz for the times of both 3, 5 and 7"{
         Say 105 | Should Be "FizzBuzzWhizz"
+        Say 210 | Should Be "FizzBuzzWhizz"
     }    
 }
